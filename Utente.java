bfd103dcfa41fe255bd900376da870e5e47fcb4e
Danilo Mosca <danilo.mosca85@gmail.com>
2026-04-28 15:55:43 +0200

    added class Utente
    added class Utente

---
+++ Utente.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Utente {
+    private String nome;
+    private String idUtente;
+    // Lista delle risorse prese in prestito:
+    private ArrayList<Risorsa> prestiti = new ArrayList<>();
+
+    // Costruttore:
+    public Utente(String nome, String idUtente) {
+        this.nome = nome;
+        this.idUtente = idUtente;
+    }
+
+    // Metodo prendi in prestito:
+    public void prendiInPrestito(Risorsa r) {
+        prestiti.add(r);
+    }
+
+    // Metodo restituisci:
+    public void restituisci(Risorsa r) {
+        prestiti.remove(r);
+    }
+
+    public void stampaPrestiti() {
+        System.out.println("Prestiti di " + nome);
+        for (Risorsa risorsa : prestiti) {
+            System.out.println(risorsa.visualizzaDettagli()); 
+        }
+    }
+}
